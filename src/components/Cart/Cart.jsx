8bcd3cc820233b44d2109ccf36b44624558470ff
DeTanvir <tanvirahmed14.ta@gmail.com>
2023-04-10 19:54:35 +0600

    conditional rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
     let message;
-    if(cart.length === 0){
+    if (cart.length === 0) {
         message = <p>Please add something.</p>
     }
-    else{
+    else {
         message = <div>
-            <h3>Borolox</h3>
+            <h3  className={`blue bold ${cart.length === 2 ? 'red' : 'green'}`}>Borolox</h3>
             <p><small>Thanks for giving your money.</small></p>
         </div>
     }
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
+            <h2 className={cart.length === 3 ? 'orange' : 'red'}>Order Summary: {cart.length}</h2>
+            {
+                cart.length > 2
+                    ? <span>Aro kino</span>
+                    : <span>Fokira</span>
+            }
             {
                 message
             }
@@ -25,6 +31,12 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                         onClick={() => handleRemoveFromCart(tshirt._id)}
                     >X</button></p>)
             }
+            {
+                cart.length === 2 && <p>Double Bonanza!!!</p>
+            }
+            {
+                cart.length === 3 || <h3>Tinta to hoilo na!!</h3>
+            }
         </div>
     );
 };
@@ -34,4 +46,13 @@ export default Cart;
 /**
  * CONDITIONAL RENDERING
  * 1. BY using if else
+ * 2. ternary operator: condition ? 'for true' : 'for false'
+ * 3. Logical && : (it works if and only the condition is true)
+ * 4. Logical || : (it works if and only the condition is false)
+ */
+
+/**
+ * CONDITIONAL CSS class
+ * 1. use ternary: add class only by condition
+ * 2. inside template string: use default class and adding class by condition inside template string
  */
